3120ebe86b83e05d7ca791176a0dd7f095e84487
mohamedGaber2004 <mg7357432@gmail.com>
2025-11-13 19:21:20 +0200

    requirments added successfully

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name="mdeical-chatbot" , 
+    version="0.1.0" ,
+    author="Mohamed Gaber Sayed" ,
+    author_email="mg7357432@gamil.com" , 
+    packages=find_packages() , 
+    install_requires = []
+)
