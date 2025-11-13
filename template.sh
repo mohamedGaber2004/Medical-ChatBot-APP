cdcc58b362d9fa92b970f3e60b92313f1322800f
Mohamed Gaber <mg7357432@gmail.com>
2025-11-13 14:52:13 +0200

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Create project directories
+mkdir -p src 
+mkdir -p research
+
+# Creating Files 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompts.py
+touch .env
+touch requirements.txt
+touch setup.py
+touch app.py
+touch research/traials.ipynb
+
+echo "Project structure created successfully."
+
+
